4df125a343962a024e9a85d87dca5799b8e07a4d
KassArc <ayushkumaryadav2029@gmail.com>
2025-08-13 09:28:34 +0530

    Create StringPointerPrint.cpp

---
+++ StringPointerPrint.cpp
@@ -0,0 +1,31 @@
+/*Ayush Yadav
+  24070123028*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Enter a string: ";
+    cin.getline(str, 100);
+
+    char* ptr = str;
+    int length = strlen(str);
+
+    ptr = str + length - 1;
+
+    cout << "Reversed string: ";
+    while (ptr >= str) {
+        cout << *ptr;
+        ptr--;
+    }
+
+    return 0;
+}
+
+/* 
+--> OUTPUT
+ Enter a string: Ayush
+ Reversed string: hsuyA
+*/
